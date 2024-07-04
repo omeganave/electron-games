53f2bd5bf46696efef34d94cf9bb446841771dbf
omeganave <omeganave@outlook.com>
2024-07-04 17:24:40 -0230

    Number Colours

--- src/renderer.js
+++ src/renderer.js
@@ -79,6 +79,32 @@ function revealSafeCells(row, col) {
         const mineCount = countAdjacentMines(row, col);
         cell.classList.add('safe');
         cell.textContent = mineCount > 0 ? mineCount : '';
+        switch (mineCount) {
+            case 1:
+                cell.style.color = 'blue';
+                break;
+            case 2:
+                cell.style.color = 'green';
+                break;
+            case 3:
+                cell.style.color = 'red';
+                break;
+            case 4:
+                cell.style.color = 'darkblue'
+                break;
+            case 5:
+                cell.style.color = 'darkred';
+                break;
+            case 6:
+                cell.style.color = 'teal';
+                break;
+            case 7:
+                cell.style.color = 'black';
+                break;
+            case 8:
+                cell.style.color = 'gray';
+                break;
+        }
         revealedSafeCells++;
 
         if (mineCount === 0) {
